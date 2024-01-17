3dd920368cf79eae9b42f1df563f0957a820356f
NachiketaTiwari69 <nachiketatiwari56@gmail.com>
2024-01-17 21:49:54 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const nameperson = document.getElementById("name");
+const cardnumber = document.getElementById("number");
+const monthperson = document.getElementById("month");
+const yearperson = document.getElementById("year");
+const cvcperson = document.getElementById("cvc");   
+
